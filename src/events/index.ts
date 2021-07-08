b8685be88cad50a65ede655f42b7ec2ba69e1e5c
Babatunde Sanusi <swisskid95@gmail.com>
2021-07-08 11:31:19 +0100

    updates

--- src/events/index.ts
+++ src/events/index.ts
@@ -3,3 +3,4 @@ export { Publisher } from './Publisher';
 export { Subjects } from './subjects';
 export { OrderStatus } from './types/order-status';
 export * from './ticket-events';
+export * from './order-events';
---
+++ src/events/order-events.ts
@@ -0,0 +1,27 @@
+import { Subjects } from './subjects';
+import { OrderStatus } from './types/order-status';
+
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
+  data: {
+    id: string;
+    status: OrderStatus;
+    userId: string;
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    };
+  };
+}
+
+export interface OrderCancelledEvent {
+  subject: Subjects.OrderCancelled;
+  data: {
+    id: string;
+    userId: string;
+    ticket: {
+      id: string;
+    };
+  };
+}
