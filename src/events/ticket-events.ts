b294468e5002d7826e6b9fce5266ec056982e696
Babatunde Sanusi <swisskid95@gmail.com>
2021-07-08 18:25:31 +0100

    updates

--- src/events/ticket-events.ts
+++ src/events/ticket-events.ts
@@ -4,6 +4,7 @@ export interface TicketCreatedEvent {
   subject: Subjects.TicketCreated;
   data: {
     id: string;
+    version: number;
     title: string;
     price: number;
     userId: string;
@@ -14,6 +15,7 @@ export interface TicketUpdatedEvent {
   subject: Subjects.TicketUpdated;
   data: {
     id: string;
+    version: number;
     title: string;
     price: number;
     userId: string;
